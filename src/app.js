12be57362ecf6b96f88643b9bc04357fed9528cc
Roman Chuykov <chuykov95@mail.ru>
2023-12-31 23:38:06 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -54,6 +54,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.status(200).send();
+});
+
 app.use((req, res, next) => {
   const proxyHeaders = {
     Accept: `application/json, text/plain, */*`,
