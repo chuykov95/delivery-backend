ab8d5c8d69b1464e7813a28d2c501c531ee1b9c8
Roman Chuykov <rychuykov@mts.ru>
2024-03-18 20:09:35 +0300

    add  TutorialController

--- src/controllers/tutorial.controller.ts
+++ src/controllers/tutorial.controller.ts
@@ -1,17 +1,35 @@
 import { Request, Response } from "express";
+import db from "../models";
+
+const Tutorial = db.tutorials;
 
 export default class TutorialController {
   async create(req: Request, res: Response) {
-    try {
-      res.status(201).json({
-        message: "create OK",
-        reqBody: req.body,
-      });
-    } catch (err) {
-      res.status(500).json({
-        message: "Internal Server Error!",
-      });
+    // Validate request
+    if (!req.body.title) {
+      res.status(400).send({ message: "Content can not be empty!" });
+      return;
     }
+
+    // Create a Tutorial
+    const tutorial = new Tutorial({
+      title: req.body.title,
+      description: req.body.description,
+      published: req.body.published ? req.body.published : false,
+    });
+
+    // Save Tutorial in the database
+    tutorial
+      .save(tutorial)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Tutorial.",
+        });
+      });
   }
 
   async findAll(req: Request, res: Response) {
